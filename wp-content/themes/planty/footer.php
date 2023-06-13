84343deb95d71bcec9e7f251b78558f160b7612e
Valentin.G <val.dac@live.fr>
2023-06-13 16:29:41 +0200

    Création et personnalisation du footer.php

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,12 @@
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+
+	<footer>
+		<a href="<?php echo esc_url( home_url( '/index.php/mentions-legales/' ) ); ?>">Mentions légales</a>	
+	</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -9,23 +9,24 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<header>
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
-	<nav>
-		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'liste-nav') ); ?>
-		<ul class="liste-nav">
-			<!-- Reprise de l'URL de page d'accueil et personnalisation du chemin pour chaque page -->
-			<li><a href="<?php echo esc_url( home_url( '/index.php/nous-rencontrer/' ) ); ?>">Nous rencontrer</a></li>
-			<!-- Ajout d'un hook permettant de rendre ce lien visible seulement si la personne est connectée à WordPress -->
-			<?php if ( is_user_logged_in() ) { ?>
-				<!-- Reprise de l'URL Admin de WordPress -->
-      			<li><a href="<?php echo esc_url( admin_url() ); ?>">Admin</a></li>
-      		<?php } ?>
-			<!-- Attribution d'un ID pour faciliter le CSS -->
-			<li id="commander"><a href="<?php echo esc_url( home_url( '/index.php/commander/' ) ); ?>">Commander</a></li>
-		</ul>
-	</nav>
-</header>
+	<header>
+		<?php get_template_part( 'template-parts/header/site-header' ); ?>
+		<nav>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'liste-nav') ); ?>
+			<ul class="liste-nav">
+				<!-- Reprise de l'URL de page d'accueil et personnalisation du chemin pour chaque page -->
+				<li><a href="<?php echo esc_url( home_url( '/index.php/nous-rencontrer/' ) ); ?>">Nous rencontrer</a></li>
+				<!-- Ajout d'un hook permettant de rendre ce lien visible seulement si la personne est connectée à WordPress -->
+				<?php if ( is_user_logged_in() ) { ?>
+					<!-- Reprise de l'URL Admin de WordPress -->
+					<li><a href="<?php echo esc_url( admin_url() ); ?>">Admin</a></li>
+				<?php } ?>
+				<!-- Attribution d'un ID pour faciliter le CSS -->
+				<li id="commander"><a href="<?php echo esc_url( home_url( '/index.php/commander/' ) ); ?>">Commander</a></li>
+			</ul>
+		</nav>
+	</header>
+	
 	<a class="skip-link screen-reader-text" href="#content"></a>
 
 	<div id="content" class="site-content">
