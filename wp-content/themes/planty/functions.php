7ff2d5d13ae3b2a3b92f5bf10df9abb3152ea77d
Valentin.G <val.dac@live.fr>
2023-06-18 01:59:27 +0200

    Modification du header.php, ajout d'un hook dans functions.php et structuration des pages

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,7 +5,22 @@
         // Chargement du CSS du thème parent
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
           // Chargement du CSS du thème enfant
-          wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), 
-    filemtime(get_stylesheet_directory() . '/css/theme.css'));
+          wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
     }
+?>
+
+<?php
+    function hook_admin() {
+        // Condition de connexion
+        if ( ! is_user_logged_in() ) {
+?>
+            <style>
+                #menu-item-374 {
+                    display: none !important;
+                }
+            </style>
+<?php
+        }
+    }
+    add_action( 'wp_head', 'hook_admin' );
 ?>
